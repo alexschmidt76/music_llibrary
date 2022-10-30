e0432be1ee5b1cda3599b7d1a0b71bb082132a5e
Alex Schmidt <alex.schmidt767@gmail.com>
2022-10-30 11:36:31 -0500

    adding useRef for searchbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
+
 import SearchBar from './components/SearchBar';
 import Gallery from './components/Gallery';
 import { DataContext } from './context/DataContext';
+import { SearchContext } from './context/SearchContext';
 
 function App() {
   let [search, setSearch] = useState('');
   let [message, setMessage] = useState('Search for Music!');
   let [data, setData] = useState([]);
+  let searchInput = useRef('');
 
   const API_URL = 'https://itunes.apple.com/search?term=';
 
@@ -35,7 +38,12 @@ function App() {
 
   return (
     <div className='App'>
-      <SearchBar handleSearch={handleSearch} />
+      <SearchContext.Provider value={{
+        term: searchInput,
+        handleSearch: handleSearch
+      }}>
+        <SearchBar />
+      </SearchContext.Provider>
       {message}
       <DataContext.Provider value={data}>
         <Gallery />
